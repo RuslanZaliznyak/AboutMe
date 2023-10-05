68964dc838633bdec08a319b78c118c1df218ec2
Ruslan Sydir <rsydir@gmail.com>
2023-10-05 19:37:54 +0200

    pop up window add

---
+++ wwwroot/js/new-record-popup.js
@@ -0,0 +1,25 @@
+﻿var addButton = document.querySelector('.btn.add');
+
+addButton.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('GET', '/Blog/Add', true);
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            
+            var popupDiv = document.createElement('div');
+            popupDiv.className = 'popup';
+            popupDiv.innerHTML = xhr.responseText;
+            document.body.appendChild(popupDiv);
+        }
+    };
+
+    
+    xhr.send();
+});
+
+
+
